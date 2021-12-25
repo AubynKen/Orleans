a44f6cb35e131ecd9db90172375e7e030ef9bdd9
Pinglei He <pingleihe@outlook.com>
2021-12-25 17:33:52 +0100

    fixed an async bug by adding await

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,29 +5,27 @@ import {
   PRODUCT_LIST_FAIL,
 } from "../constants/productConstatns";
 
-export const productList = () => {
-  return async (dispatch) => {
-    try {
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_LIST_REQUEST,
+    }); // tells the redux reducer that we've initiated a request
+    const { data } = await axios.get("/api/products");
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    }); // sends products to the reducer
+  } catch (err) {
+    if (err.response && err.response.data.message) {
       dispatch({
-        type: PRODUCT_LIST_REQUEST,
-      }); // tells the redux reducer that we've initiated a request
-      const { data } = axios.get("/api/products");
+        type: PRODUCT_LIST_FAIL,
+        payload: err.response.data.message, // axios-raised error
+      });
+    } else {
       dispatch({
-        type: PRODUCT_LIST_SUCCESS,
-        payload: data,
-      }); // sends products to the reducer
-    } catch (err) {
-      if (err.response && err.response.data.message) {
-        dispatch({
-          type: PRODUCT_LIST_FAIL,
-          payload: err.response.data.message, // axios-raised error
-        });
-      } else {
-        dispatch({
-          type: PRODUCT_LIST_FAIL,
-          payload: err.message, // generic error
-        });
-      }
+        type: PRODUCT_LIST_FAIL,
+        payload: err.message, // generic error
+      });
     }
-  };
+  }
 };
