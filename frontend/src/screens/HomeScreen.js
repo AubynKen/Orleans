83d174ee9b9cd3e8bacd42dd68eae2e4a640dc60
Pinglei He <pingleihe@outlook.com>
2021-12-25 18:28:18 +0100

    Incorporated redux into HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,40 +1,41 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Container, Row, Col } from "react-bootstrap";
 import ProductCard from "../components/ProductCard";
+import { listProducts } from "../actions/productActions";
 import axios from "axios";
 
-export default class HomeScreen extends Component {
-  constructor() {
-    super();
-    this.state = {
-      products: [],
-    };
-  }
+const HomeScreen = () => {
+  const dispatch = useDispatch();
 
-  fetchProducts = async () => {
-    const { data } = await axios.get("/api/products");
-    return data;
-  };
+  const { products, error, loading } = useSelector(
+    (state) => state.productList
+  );
 
-  async componentDidMount() {
-    const products = await this.fetchProducts();
-    this.setState({ products: products });
-  }
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-  render() {
-    return (
-      <>
-        <h1 style={{ fontWeight: "200" }}>Nos derniers produits</h1>
+  return (
+    <>
+      <h1 style={{ fontWeight: "200" }}>Nos derniers produits</h1>
+      {loading ? (
+        <h2>En train de charger...</h2>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
         <Container>
           <Row>
-            {this.state.products.map((el) => (
+            {products.map((el) => (
               <Col sm="12" md="6" lg="4" key={el._id}>
                 <ProductCard product={el} />
               </Col>
             ))}
           </Row>
         </Container>
-      </>
-    );
-  }
-}
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
